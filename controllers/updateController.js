1062fc81313b7ae1d3375c61dc64f28d8d838128
Rishant Mastwal <rishantmastwal@gmail.com>
2023-06-05 13:53:40 +0530

    some fixes for prev commit

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -47,16 +47,24 @@ module.exports.createTaskUpdate=async(req,res,next)=>{
     try{
         const {taskId}=req.params;
         const {updateReason,committedDate,updateStatus}=req.body;
-        const task=await Task.findById(taskId)
-        if(!task.assignTo.concat([task.author].includes(req.userID))){
+        const task=await Task.findById(taskId).populate('author','-password')
+        const loggedUser = await User.findById(req.userID)
+        if(!(task.assignTo.concat([task.author.id]).includes(req.userID) || (loggedUser?.role==='department head' && loggedUser?.dept===task.author?.dept))){
             return res.status(403).json({message:'You are not allowed to update status'})
         }
         const newUpdate=new TaskUpdate({taskId,updateReason,updatedBy:req.userID,committedDate,updateStatus})
         newUpdate.attachment=req?.files?.map(f=>({url:f.path,filename:f.filename}))
         const updatedTask=await Task.findByIdAndUpdate(taskId,{$push:{taskUpdates:newUpdate._id},status:updateStatus},{new:true}).populate('author');
         const updateAuthor=await User.findById(req.userID)
-        if(newUpdate.updatedBy.equals(task.author)){
+        if(newUpdate.updatedBy.equals(task.author._id)){
             task.assignTo.forEach(async(user)=>{
+                const notifys=new Notification({recipient:user._id,taskUpdate:newUpdate._id})
+                await notifys.save()
+                const userEmail=await User.findById(user._id).select({'email':1,_id:0})
+                sendMail(taskUpdateEmail(updateAuthor.name,userEmail,updatedTask,newUpdate))
+            })
+        }else if(!task.assignTo.includes(req.userID) && loggedUser.role === 'department head'){
+            task.assignTo.concat([task.author]).forEach(async(user)=>{
                 const notifys=new Notification({recipient:user._id,taskUpdate:newUpdate._id})
                 await notifys.save()
                 const userEmail=await User.findById(user._id).select({'email':1,_id:0})
