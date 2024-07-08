94e6876217688b15afb8bd1b1df002c3c008bebf
Rishant Mastwal <rishantmastwal@gmail.com>
2024-07-08 13:24:16 +0530

    env changes

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGO_URI_DEV);
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${connection.host} - ${connection.name}`);
   } catch (err) {
     console.log(`Error: ${err.message}`);
